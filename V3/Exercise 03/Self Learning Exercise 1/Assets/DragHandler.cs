981dd4a5b09b1118638b72b89824732e28247f7d
GauravSharma002 <43457285+GauravSharma002@users.noreply.github.com>
2018-09-29 01:17:49 -0600

    Added a new script to the Project

---
+++ V3/Exercise 03/Self Learning Exercise 1/Assets/DragHandler.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DragHandler : MonoBehaviour, IDragHandler {
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        transform.position = Input.mousePosition;
+    }
+
+    //public void OnEndDrag(PointerEventData eventData)
+    //{
+    //    transform.position = new Vector3();
+    //}
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
