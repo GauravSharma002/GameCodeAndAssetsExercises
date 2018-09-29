493532c5b28a4261c12ce739856e2d5e13c998ab
GauravSharma002 <43457285+GauravSharma002@users.noreply.github.com>
2018-09-29 00:34:16 -0600

    These are the Files for Exercise 03

---
+++ V3/Exercise 03/Self Learning Exercise 1/Assets/BoxColor.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BoxColor : MonoBehaviour
+{
+    public void TurnRed()
+    {
+        gameObject.GetComponent<RawImage>().material.color = Color.red;
+    }
+
+    public void TurnOriginal()
+    {
+        gameObject.GetComponent<RawImage>().material.color = Color.white;
+    }
+}
---
+++ V3/Exercise 03/Self Learning Exercise 1/Assets/BoxSlider.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BoxSlider : MonoBehaviour
+{
+    public Slider slider;
+
+    public void SliderScale()
+    {
+        transform.localScale = new Vector3(slider.value, slider.value, slider.value);
+    }
+}
